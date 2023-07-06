8a072fa034afa137ef4acbbac7c65dcfeddf63b3
Prathm <jawahireprathmesh@gmail.com>
2023-07-06 17:03:26 +0530

    Email Validation

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -19,7 +19,7 @@ public class UserRegistration {
         else {
             System.out.println("Sorry! your First name is not valid.");
         }
-    }*/
+    }
     public void validlastname()
     {
         Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
@@ -35,13 +35,31 @@ public class UserRegistration {
         else {
             System.out.println("Sorry! your last name is not valid.");
         }
+    }*/
+    public void validEmail()
+    {
+        Pattern pattern = Pattern.compile("^[a-z]*.[a-z]+@[a-z]+.[a-z]{2,3}(.[a-z]{2})*$");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your mail:");
+        String mail=sc.nextLine();
+
+        Matcher matcher=pattern.matcher(mail);
+        if(matcher.matches())
+        {
+            System.out.println("Nice! your Email is valid.");
+        }
+        else {
+            System.out.println("Sorry! your Email is not valid.");
+        }
     }
 
     public static void main(String[] args)
     {
         UserRegistration ur = new UserRegistration();
         //ur.validfirstname();
-        ur.validlastname();
+        //ur.validlastname();
+        ur.validEmail();
+
     }
 
 }
